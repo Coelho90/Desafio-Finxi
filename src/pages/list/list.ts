322b92f6f54ddd4cecbbbbcc7eac635d4870ebd0
Coelho90 <henriquebc90@gmail.com>
2017-08-08 00:17:44 -0300

    Bugfix Storage
    problemas de persistencia

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -19,37 +19,39 @@ export class MesasPage {
   items: Array<{title: string, icon: string, status: boolean}>;
   numMesas: number;
   
-
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
 
     this.icons = ['ios-restaurant'];
     this.items = []; 
     this.storage.length().then(result => { this.numMesas = result});
     this.storage.length().then(result => { 
-      for (let i = 1; i < result; i++) {
+      for (let i = 1; i <= result; i++) {
         this.storage.get('Mesa '+ i).then(result => { this.items.push({title: 'Mesa ' + i, icon: this.icons[0], status: result })});
     }});
-    
-
   }
 
+
+  // altera a flag da mesa (ocupada/disponível)
   changeStatus(i: number){
     this.items[i].status = !this.items[i].status;
     this.storage.set(this.items[i].title, this.items[i].status); 
   }
   
+
+  // adiciona uma mesa à lista de mesas
   addMesa(){
-    
+    this.numMesas = this.numMesas + 1;
     this.items.push({
-      title: 'Mesa ' + String(this.numMesas + 1),
+      title: 'Mesa ' + this.numMesas,
       icon: this.icons[0],
       status: true
     });
-    this.storage.set(this.items[this.numMesas].title, this.items[this.numMesas].status);
-    this.numMesas = this.numMesas + 1;
+    this.storage.set(this.items[this.numMesas -1].title, this.items[this.numMesas -1].status);
+    
     
   }
 
+  // remove a última mesa da lista de mesas
   removeMesa(){
     this.items.pop();
     this.storage.remove('Mesa ' + this.numMesas);
@@ -57,6 +59,4 @@ export class MesasPage {
       this.numMesas = this.numMesas - 1;
     }  
   }
-
-
 }
